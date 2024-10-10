1ca26f60ad5acee9de4bdfb66dd5e814d2d07be6
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-10-10 06:56:12 +0300

    20241010 added metadata

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from . import __version__
 
-app = FastAPI()
+app = FastAPI(
+    title="switcher",
+    version=__version__,
+    license_info={
+        "name": "GNU General Public License v3.0 or later",
+        "identifier": "GPL-3.0-or-later"
+        }
+    )
 
 
 @app.get("/")
