bce8756a72a7cbfa293fe8df175be00680719442
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-10-19 06:47:17 +0300

    20241019 added app/database.py

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, create_engine
+from app.config import settings
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(settings.database_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
