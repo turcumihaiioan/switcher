163dd2afa593b3c2a5d8bd2e137d362b9bdee94f
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-10-20 06:39:26 +0300

    20241020 linted and added database app/main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from .config import settings
-from . import __version__
+from app.config import settings
+from app import __version__
+from app.database import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
 
 app = FastAPI(
+    lifespan=lifespan,
     title="switcher",
     version=__version__,
     license_info={
         "name": "GNU General Public License v3.0 or later",
-        "identifier": "GPL-3.0-or-later"
-        }
+        "identifier": "GPL-3.0-or-later",
+        },
     )
 
 
@@ -16,9 +26,10 @@ app = FastAPI(
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/info")
 async def info():
     return {
         "settings": settings,
-        "database_url": settings.database_url
+        "database_url": settings.database_url,
         }
