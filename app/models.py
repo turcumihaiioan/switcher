3ba454a14878b71047a697e636df67efeb5e7d0c
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-10-27 07:11:18 +0200

    20241027 updated app/models.py to index

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field, SQLModel
 
 class UserBase(SQLModel):
-    username: str | None = Field(default=None, max_length=150)
+    username: str | None = Field(unique=True, index=True, max_length=150)
     name: str | None = Field(default=None, max_length=150)
     is_active: bool = True
 
@@ -9,7 +9,7 @@ class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
 class GroupBase(SQLModel):
-    name: str = Field(unique=True, max_length=150)
+    name: str = Field(unique=True, index=True, max_length=150)
 
 class Group(GroupBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -17,4 +17,3 @@ class Group(GroupBase, table=True):
 class Group_User(SQLModel, table=True):
     group_id: int | None = Field(default=None, foreign_key="group.id", primary_key=True)
     user_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
-
