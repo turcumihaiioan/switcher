ebc201190ce92f17af3da242925a8319269b569c
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-11-20 06:59:22 +0200

    20241120 updated app/routers/group.py to read group by id to update group and to delete group

--- app/routers/group.py
+++ app/routers/group.py
@@ -6,6 +6,7 @@ from app.models.group import (
     Group,
     GroupCreate,
     GroupPublic,
+    GroupUpdate,
 )
 
 router = APIRouter()
@@ -31,3 +32,44 @@ def create_group(*, session: SessionDep, group: GroupCreate):
 def read_group(*, session: SessionDep):
     groups = session.exec(select(Group)).all()
     return groups
+
+
+@router.get("/{group_id}", response_model=GroupPublic)
+def read_group_by_id(*, session: SessionDep, group_id: int):
+    db_group = session.get(Group, group_id)
+    if not db_group:
+        raise HTTPException(
+            status_code=404,
+            detail="The group with this id does not exist in the system",
+        )
+    return db_group
+
+
+@router.patch("/{group_id}", response_model=GroupPublic)
+def update_group(*, session: SessionDep, group_id: int, group: GroupUpdate):
+    db_group = session.get(Group, group_id)
+    if not db_group:
+        raise HTTPException(
+            status_code=404,
+            detail="The group with this id does not exist in the system",
+        )
+    group_data = group.model_dump(exclude_unset=True)
+    for key, value in group_data.items():
+        setattr(db_group, key, value)
+    session.add(db_group)
+    session.commit()
+    session.refresh(db_group)
+    return db_group
+
+
+@router.delete("/{group_id}")
+def delete_group(session: SessionDep, group_id: int):
+    db_group = session.get(Group, group_id)
+    if not db_group:
+        raise HTTPException(
+            status_code=404,
+            detail="The group with this id does not exist in the system",
+        )
+    session.delete(db_group)
+    session.commit()
+    return {"ok": True}
