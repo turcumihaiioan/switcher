ec2e32bd61b9cff0e1313b64139c2ba59e8a74bf
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-11-23 06:35:09 +0200

    20241123 updated app/models.py with ondelete cascade parameter

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from sqlmodel import Field, SQLModel
 
 
 class Group_User(SQLModel, table=True):
-    group_id: int | None = Field(default=None, foreign_key="group.id", primary_key=True)
-    user_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
+    group_id: int | None = Field(default=None, foreign_key="group.id", ondelete="CASCADE", primary_key=True)
+    user_id: int | None = Field(default=None, foreign_key="user.id", ondelete="CASCADE", primary_key=True)
 
 
 class GroupBase(SQLModel):
