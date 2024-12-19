0875ed867db1454a1c12087b3de92a393af80920
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-12-19 06:51:59 +0200

    20241219 updated app/routers/venv.py get respones_model with VenvPublic

--- app/routers/venv.py
+++ app/routers/venv.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter
 from sqlmodel import select
 
 from app.database import SessionDep
-from app.models import Venv
+from app.models import (
+    Venv,
+    VenvPublic,
+)
 
 router = APIRouter()
 
 
-@router.get("", response_model=list[Venv])
+@router.get("", response_model=list[VenvPublic])
 def read_venv(*, session: SessionDep):
     venvs = session.exec(select(Venv)).all()
     return venvs
