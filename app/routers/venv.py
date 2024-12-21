bfc7b6ef16ac69867f467179c40f5a7922344303
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2024-12-21 07:02:15 +0200

    20241221 updated app/routers/venv.py to create venv

--- app/routers/venv.py
+++ app/routers/venv.py
@@ -1,13 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
 from app.database import SessionDep
 from app.models import (
     Venv,
+    VenvCreate,
     VenvPublic,
 )
 
 router = APIRouter()
+
+
+@router.post("", response_model=VenvPublic)
+def create_venv(*, session: SessionDep, venv: VenvCreate):
+    statement = select(Venv).where(Venv.name == venv.name)
+    db_venv = session.exec(statement).first()
+    if db_venv:
+        raise HTTPException(
+            status_code=400,
+            detail="The venv with this name already exists in the system",
+        )
+    db_venv = Venv.model_validate(venv)
+    session.add(db_venv)
+    session.commit()
+    session.refresh(db_venv)
+    return db_venv
 
 
 @router.get("", response_model=list[VenvPublic])
