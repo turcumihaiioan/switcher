966c93b07edb338bdf063787d94cd1e60f7e9ab4
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-01-13 06:53:31 +0200

    20250113 updated app/routers/venv_package.py to create venv_package

--- app/routers/venv_package.py
+++ app/routers/venv_package.py
@@ -4,10 +4,20 @@ from sqlmodel import select
 from app.database import SessionDep
 from app.models import (
     Venv_Package,
+    Venv_PackageCreate,
     Venv_PackagePublic,
 )
 
 router = APIRouter()
+
+
+@router.post("", response_model=Venv_PackagePublic)
+def create_venv_package(*, session: SessionDep, venv_package: Venv_PackageCreate):
+    db_venv_package = Venv_Package.model_validate(venv_package)
+    session.add(db_venv_package)
+    session.commit()
+    session.refresh(db_venv_package)
+    return db_venv_package
 
 
 @router.get("", response_model=list[Venv_PackagePublic])
