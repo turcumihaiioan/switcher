802020031ca6dedb2b4d174c1442f79fdc649b5f
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-01-14 06:51:51 +0200

    20250114 updated app/database.py to run PRAGMA foreign_keys=ON for sqlite

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import SQLModel, Session, create_engine, text
 
 from app.config import settings
 
@@ -11,6 +11,9 @@ engine = create_engine(settings.database_url, echo=True, connect_args=connect_ar
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+    if settings.database_type == "sqlite":
+        with engine.connect() as connection:
+            connection.execute(text("PRAGMA foreign_keys=ON"))
 
 
 def get_session():
