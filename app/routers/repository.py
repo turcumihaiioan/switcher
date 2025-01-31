2a1169d18820e2236e56c38cfba382fe1f777dbc
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-01-31 06:47:33 +0200

    20250131 updated app/routers/repository.py to read repository by id

--- app/routers/repository.py
+++ app/routers/repository.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
 from app.database import SessionDep
@@ -14,3 +14,14 @@ router = APIRouter()
 def read_repository(*, session: SessionDep):
     repositories = session.exec(select(Repository)).all()
     return repositories
+
+
+@router.get("/{repository_id}", response_model=RepositoryPublic)
+def read_repository_by_id(*, session: SessionDep, repository_id: int):
+    db_repository = session.get(Repository, repository_id)
+    if not db_repository:
+        raise HTTPException(
+            status_code=404,
+            detail="The repository with this id does not exist in the system",
+        )
+    return db_repository
