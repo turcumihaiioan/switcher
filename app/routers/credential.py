bb534357c55857e04f8d6185193d899336515357
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-02-17 06:46:50 +0200

    20250217 updated app/routers/credential.py to update credential

--- app/routers/credential.py
+++ app/routers/credential.py
@@ -6,6 +6,7 @@ from app.models import (
     Credential,
     CredentialCreate,
     CredentialPublic,
+    CredentialUpdate,
 )
 
 router = APIRouter()
@@ -41,4 +42,32 @@ def read_credential_by_id(*, session: SessionDep, credential_id: int):
             status_code=404,
             detail="The credential with this id does not exist in the system",
         )
+    return db_credential
+
+
+@router.patch("/{credential_id}", response_model=CredentialPublic)
+def update_credential(
+    *, session: SessionDep, credential_id: int, credential: CredentialUpdate
+):
+    db_credential = session.get(Credential, credential_id)
+    if not db_credential:
+        raise HTTPException(
+            status_code=404,
+            detail="The credential with this id does not exist in the system",
+        )
+    credential_data = credential.model_dump(exclude_unset=True)
+    for key, value in credential_data.items():
+        if key == "name":
+          statement = select(Credential).where(Credential.id != credential_id, Credential.name == value)
+          db_credential_name = session.exec(statement).first()
+          print (db_credential_name)
+          if db_credential_name:
+            raise HTTPException(
+                status_code=404,
+                detail="The credential with this name already exists in the system",
+            )
+        setattr(db_credential, key, value)
+    session.add(db_credential)
+    session.commit()
+    session.refresh(db_credential)
     return db_credential
