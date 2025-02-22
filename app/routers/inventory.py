b5a8deacc7eac48b1da6166f24956be1609da859
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-02-22 06:44:40 +0200

    20250222 updated app/routers/inventory.py to read inventory by id

--- app/routers/inventory.py
+++ app/routers/inventory.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
 from app.database import SessionDep
@@ -14,3 +14,14 @@ router = APIRouter()
 def read_inventory(*, session: SessionDep):
     inventories = session.exec(select(Inventory)).all()
     return inventories
+
+
+@router.get("/{inventory_id}", response_model=InventoryPublic)
+def read_inventory_by_id(*, session: SessionDep, inventory_id: int):
+    db_inventory = session.get(Inventory, inventory_id)
+    if not db_inventory:
+        raise HTTPException(
+            status_code=404,
+            detail="The inventory with this id does not exist in the system",
+        )
+    return db_inventory
