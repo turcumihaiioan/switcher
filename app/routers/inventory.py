6cac050e8a0996fbba039227249ff70d8b9b1189
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-02-26 06:48:04 +0200

    20250226 updated app/routers/inventory.py to update inventory

--- app/routers/inventory.py
+++ app/routers/inventory.py
@@ -6,6 +6,7 @@ from app.models import (
     Inventory,
     InventoryCreate,
     InventoryPublic,
+    InventoryUpdate,
 )
 
 router = APIRouter()
@@ -41,4 +42,32 @@ def read_inventory_by_id(*, session: SessionDep, inventory_id: int):
             status_code=404,
             detail="The inventory with this id does not exist in the system",
         )
+    return db_inventory
+
+
+@router.patch("/{inventory_id}", response_model=InventoryPublic)
+def update_inventory(
+    *, session: SessionDep, inventory_id: int, inventory: InventoryUpdate
+):
+    db_inventory = session.get(Inventory, inventory_id)
+    if not db_inventory:
+        raise HTTPException(
+            status_code=404,
+            detail="The inventory with this id does not exist in the system",
+        )
+    inventory_data = inventory.model_dump(exclude_unset=True)
+    for key, value in inventory_data.items():
+        if key == "name":
+          statement = select(Inventory).where(Inventory.id != inventory_id, Inventory.name == value)
+          db_inventory_name = session.exec(statement).first()
+          print (db_inventory_name)
+          if db_inventory_name:
+            raise HTTPException(
+                status_code=404,
+                detail="The inventory with this name already exists in the system",
+            )
+        setattr(db_inventory, key, value)
+    session.add(db_inventory)
+    session.commit()
+    session.refresh(db_inventory)
     return db_inventory
