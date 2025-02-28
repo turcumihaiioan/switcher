2c0748ab661080729fe9b465b19fc4ee4fda78df
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-02-28 06:52:40 +0200

    20250228 moved paths to app/api/routers and added app/api/main.py

---
+++ app/api/main.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+
+from app.config import settings
+
+from app.api.routers import user, group, venv, venv_package, repository, credential, inventory
+
+router = APIRouter()
+
+
+@router.get("/info")
+async def info():
+    return {
+        "settings": settings,
+        "database_url": settings.database_url,
+    }
+
+
+router.include_router(user.router, prefix="/user", tags=["user"])
+router.include_router(group.router, prefix="/group", tags=["group"])
+router.include_router(venv.router, prefix="/venv", tags=["venv"])
+router.include_router(venv_package.router, prefix="/venv_package", tags=["venv_package"])
+router.include_router(repository.router, prefix="/repository", tags=["repository"])
+router.include_router(credential.router, prefix="/credential", tags=["credential"])
+router.include_router(inventory.router, prefix="/inventory", tags=["inventory"])
--- app/main.py
+++ app/main.py
@@ -3,9 +3,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app import __version__
-from app.config import settings
 from app.database import create_db_and_tables
-from app.routers import user, group, venv, venv_package, repository, credential, inventory
+from app.api.main import router as api_router
 
 
 @asynccontextmanager
@@ -24,19 +23,4 @@ app = FastAPI(
     },
 )
 
-
-@app.get("/info")
-async def info():
-    return {
-        "settings": settings,
-        "database_url": settings.database_url,
-    }
-
-
-app.include_router(user.router, prefix="/user", tags=["user"])
-app.include_router(group.router, prefix="/group", tags=["group"])
-app.include_router(venv.router, prefix="/venv", tags=["venv"])
-app.include_router(venv_package.router, prefix="/venv_package", tags=["venv_package"])
-app.include_router(repository.router, prefix="/repository", tags=["repository"])
-app.include_router(credential.router, prefix="/credential", tags=["credential"])
-app.include_router(inventory.router, prefix="/inventory", tags=["inventory"])
+app.include_router(api_router, prefix="/api")
