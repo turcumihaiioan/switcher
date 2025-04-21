5fc180e575b44afc1349c4b90a7df3eac5708a1b
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-04-21 06:47:31 +0300

    20250421 updated app/api/routers/journal.py to read journal by id

--- app/api/routers/journal.py
+++ app/api/routers/journal.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+import uuid
+
+from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
 from app.database import SessionDep
 from app.models import (
     Journal,
     JournalPublic,
+    JournalPublicWithMessages,
 )
 
 
@@ -15,3 +18,14 @@ router = APIRouter()
 def read_journal(*, session: SessionDep):
     journals = session.exec(select(Journal)).all()
     return journals
+
+
+@router.get("/{journal_id}", response_model=JournalPublicWithMessages)
+def read_journal_by_id(*, session: SessionDep, journal_id: uuid.UUID):
+    db_journal = session.get(Journal, journal_id)
+    if not db_journal:
+        raise HTTPException(
+            status_code=404,
+            detail="The journal with this id does not exist in the system",
+        )
+    return db_journal
