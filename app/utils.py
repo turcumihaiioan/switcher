fda1956bc681511c67bfaba3ba87c2151fb4abdd
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-05-06 06:44:17 +0300

    20250506 updated app/utils.py with create_journal_message

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,10 @@
+from fastapi import HTTPException
 from sqlmodel import Session
 from app.models import (
     Journal,
     JournalCreate,
+    Journal_Message,
+    Journal_MessageCreate,
 )
 
 
@@ -11,3 +14,15 @@ def create_journal(*, session: Session, journal: JournalCreate):
     session.commit()
     session.refresh(db_journal)
     return db_journal.id
+
+
+def create_journal_message(*, session: Session, journal_message: Journal_MessageCreate):
+    db_journal = session.get(Journal, journal_message.journal_id)
+    if not db_journal:
+        raise HTTPException(
+            status_code=400,
+            detail="The journal with this id does not exists in the system",
+        )
+    db_journal_message = Journal_Message.model_validate(journal_message)
+    session.add(db_journal_message)
+    session.commit()
