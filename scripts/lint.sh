42fc3ca9d261b01a9280ce4928678ea36214e65d
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-05-23 06:55:33 +0300

    20250523 updated scripts/lint.sh with yamllint ansible-lint and syntax-check for app/playbooks

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,5 +4,11 @@ set -e
 set -x
 
 mypy app --allow-redefinition
+
 ruff check app
 ruff format app --check
+
+yamllint app/playbooks
+
+ansible-lint app/playbooks
+ansible-playbook --inventory localhost, --syntax-check app/playbooks/*
