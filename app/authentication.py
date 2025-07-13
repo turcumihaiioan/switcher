c4ae0804ce3b1f9f786df86d77fdd83c87d3a50e
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-07-13 06:51:01 +0300

    20250713 updated app/authentication.py with ouath2_scheme tokenUrl

--- app/authentication.py
+++ app/authentication.py
@@ -3,6 +3,6 @@ from typing import Annotated
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/login/access-token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/token")
 
 TokenDep = Annotated[str, Depends(oauth2_scheme)]
