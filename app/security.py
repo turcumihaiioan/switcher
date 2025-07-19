028abaeea0ffae1e0968501410e793207faa9c4e
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-07-19 06:50:00 +0300

    20250719 updated app/security.py with create_token

--- app/security.py
+++ app/security.py
@@ -1,6 +1,11 @@
 import bcrypt
+import jwt
 
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
+
+from app.config import settings
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -11,3 +16,11 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def create_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def create_token(data: dict, expires_delta: timedelta) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm="HS256")
+    return encoded_jwt
