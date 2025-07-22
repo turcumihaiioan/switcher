d629e61bf59cc36dfd85910c48d2b5c4d43b358d
turcumihaiioan <turcu.mihai.ioan@protonmail.com>
2025-07-22 06:43:52 +0300

    20250722 updated app/api/routers/auth.py to check if the user exists and is active

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -2,7 +2,7 @@ from sqlmodel import select
 from typing import Annotated
 
 from datetime import timedelta
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.config import settings
@@ -19,6 +19,10 @@ def create_access_token(
 ) -> Token:
     statement = select(User).where(User.username == form_data.username)
     db_user = session.exec(statement).first()
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    elif not db_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
     token_expires = timedelta(settings.token_expire_minutes)
     token = create_token(data={"user_id": str(db_user.id)}, expires_delta=token_expires)
     return Token(access_token=token)
